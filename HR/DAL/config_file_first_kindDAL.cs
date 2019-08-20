9ae188de76ec61f704520a393a374ea10fbee98f
Daisy-Maybe <854485670@qq.com>
2019-08-20 11:37:31 +0800

    第一次提交

---
+++ HR/DAL/config_file_first_kindDAL.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFEntity;
+using IDAL;
+using Model;
+
+namespace DAL
+{
+    public class config_file_first_kindDAL : DaoBase<config_file_first_kind>, Iconfig_file_first_kindDAL
+    {
+        public int FirstKindAdd(config_file_first_kindModel fk)
+        {
+            config_file_first_kind est = new config_file_first_kind()
+            {
+                Id = fk.Id,
+                first_kind_id = fk.first_kind_id,
+                first_kind_name = fk.first_kind_name,
+                first_kind_salary_id = fk.first_kind_salary_id,
+                first_kind_sale_id = fk.first_kind_sale_id
+            };
+            return Add(est);
+        }
+
+        public int FirstKindDel(config_file_first_kindModel fk)
+        {
+            //把DTO转为EO
+            config_file_first_kind est = new config_file_first_kind()
+            {
+                Id = fk.Id
+            };
+            return Delete(est);
+        }
+
+        public List<config_file_first_kindModel> FirstKindSelect()
+        {
+            List<config_file_first_kind> list = SelectAll();
+            List<config_file_first_kindModel> list2 = new List<config_file_first_kindModel>();
+            foreach (config_file_first_kind item in list)
+            {
+                config_file_first_kindModel cm = new config_file_first_kindModel()
+                {
+                    Id = item.Id,
+                    first_kind_id = item.first_kind_id,
+                    first_kind_name = item.first_kind_name,
+                    first_kind_salary_id = item.first_kind_salary_id,
+                    first_kind_sale_id = item.first_kind_sale_id
+                };
+                list2.Add(cm);
+            }
+            return list2;
+        }
+
+        public config_file_first_kindModel FirstKindSelectBy(int id)
+        {
+            config_file_first_kind c = SelectBy(e => e.Id.Equals(id)).FirstOrDefault();
+            config_file_first_kindModel cm = new config_file_first_kindModel()
+            {
+                Id=c.Id,
+                first_kind_id=c.first_kind_id,
+                first_kind_name=c.first_kind_name,
+                first_kind_salary_id=c.first_kind_salary_id,
+                first_kind_sale_id=c.first_kind_sale_id
+            };
+            return cm;
+        }
+
+        public int FirstKindUpdate(config_file_first_kindModel fk)
+        {
+            config_file_first_kind est = new config_file_first_kind()
+            {
+                Id = fk.Id,
+                first_kind_id = fk.first_kind_id,
+                first_kind_sale_id = fk.first_kind_sale_id,
+                first_kind_name = fk.first_kind_name,
+                first_kind_salary_id = fk.first_kind_salary_id
+            };
+            return Update(est);
+        }
+    }
+}
---
+++ HR/IOC/IocCreate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Practices.Unity.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+using System.Configuration;
+using IBLL;
+using IDAL;
+using DAL;
+
+namespace IOC
+{
+    public class IocCreate
+    {
+        public static Iconfig_file_first_kindDAL Createconfig_file_first_kindModelDAL()
+        {
+            UnityContainer ioc = new UnityContainer();
+            ioc.RegisterType<Iconfig_file_first_kindDAL, config_file_first_kindDAL>();
+            return ioc.Resolve<Iconfig_file_first_kindDAL>();
+        }
+        private static UnityContainer GetBLLSeciton()
+        {
+            UnityContainer ioc = new UnityContainer();
+            ExeConfigurationFileMap ecf = new ExeConfigurationFileMap();
+            ecf.ExeConfigFilename = @"E:\HR管理系统\HR\UI\Untity.config";
+            Configuration cf = ConfigurationManager.OpenMappedExeConfiguration(ecf, ConfigurationUserLevel.None);
+            UnityConfigurationSection cfs = cf.GetSection("unity") as UnityConfigurationSection;
+            ioc.LoadConfiguration(cfs, "containerOne");
+            return ioc;
+        }
+        public static Iconfig_file_first_kindBLL Createconfig_file_first_kindBLL()
+        {
+            UnityContainer ioc = GetBLLSeciton();
+            return ioc.Resolve<Iconfig_file_first_kindBLL>("config_file_first_kindBLL");
+        }
+    }
+}
